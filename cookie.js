ea2b34d9c0fde6ba395a60d6e856b16513a8159c
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-04-18 01:37:31 +0300

    adding-a-form-closing-function

--- cookie.js
+++ cookie.js
@@ -1,6 +1,7 @@
 function wasPopupShownToday() {
     const lastShownDate = localStorage.getItem('cookiePopupShownDate');
     const today = new Date().toLocaleDateString();
+    
     return lastShownDate === today;
 }
   
@@ -9,18 +10,26 @@ function showPopup() {
     popup.classList.add('show');
 }
   
+function closePopup() {
+    const popup = document.getElementById('cookiePopup');
+    popup.classList.remove('show');
+}
+  
 function handleAcceptCookies() {
     localStorage.setItem('cookiePopupShownDate', new Date().toLocaleDateString());
+    closePopup();
 }
   
 function initializePopup() {
     const acceptCookiesBtn = document.getElementById('acceptCookies');
+    const closePopupBtn = document.getElementById('closePopup');
   
     if (!wasPopupShownToday()) {
       showPopup();
     }
   
     acceptCookiesBtn.addEventListener('click', handleAcceptCookies);
+    closePopupBtn.addEventListener('click', closePopup);
 }
   
 window.addEventListener('DOMContentLoaded', initializePopup);
